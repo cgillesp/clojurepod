91abcf6d7214b6221e4e344ab77fcef1f15c213b
Gillespie <usacfg@gmail.com>
2022-09-23 16:08:12 -0400

    changed name in html base

--- src/clojurepod/core.clj
+++ src/clojurepod/core.clj
@@ -28,7 +28,7 @@
 (rum/defc common-head [] [:head
                      [:meta {:charset "UTF-8"}]
                      [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
-                     [:title "PodSearch"]
+                     [:title "Pinkcast"]
                      [:link {:rel "stylesheet" :href "/static/style.css"}]]
   )
 ;;    <script src="cljs-out/dev-main.js"></script>
